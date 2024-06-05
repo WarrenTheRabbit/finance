9b23af2a04bd0beb6c41b7e8855aff113d3ed0b4
Warren <rabbitsinwarrens@gmail.com>
2024-06-05 11:43:22 +1000

    test(jira): include issue link in commit SCRUM-4

--- transactions/repository/transactions_repository.py
+++ transactions/repository/transactions_repository.py
@@ -1,11 +1,14 @@
+from typing import List
 from transactions.transactions_service.transactions import Transaction
-
 
 class TransactionsRepository:
     def __init__(self):
         self.database = []
-
+    
     def add(self) -> Transaction:
         transaction = Transaction()
         self.database.append(transaction)
         return transaction
+    
+    def list(self) -> List[Transaction]:
+        return self.database
--- transactions/transactions_service/transactions_service.py
+++ transactions/transactions_service/transactions_service.py
@@ -1,10 +1,17 @@
 from transactions.repository.transactions_repository import TransactionsRepository
 from transactions.transactions_service.transactions import Transaction
 
-
 class TransactionsService:
     def __init__(self, transactions_repository: TransactionsRepository):
         self.transactions_repository = transactions_repository
-
+    
+    def get_transaction(self):
+        pass
+    
     def create_transaction(self) -> Transaction:
         return self.transactions_repository.add()
+    
+    def list_transactions(self):
+        return self.transactions_repository.list()
+    
+    
